272df3fe967c474cc52a7f5b546897fa58003947
muathendirangu <muathe.ndirangu@gmail.com>
2020-03-25 03:09:14 +0000

    add room provider state globally

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {RoomProvider} from './context';
+
 ReactDOM.render(
   <React.StrictMode>
+    <RoomProvider>
     <Router>
     <App />
     </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
