b881afa09c5e3f955736a8dd25a199cf2701c189
muathendirangu <muathe.ndirangu@gmail.com>
2020-04-01 01:50:58 +0000

    add room list component

---
+++ src/components/RoomsFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomsFilter = () => {
+    return (
+        <div>
+            RoomsFilter component      
+        </div>
+    );
+}
+
+export default RoomsFilter;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner';
+import RoomsContainer from '../components/RoomsContainer';
+
 const Rooms = () => {
     return (
+        <>
         <Hero hero="roomsHero">
             <Banner title="our rooms" subtitle="deluxe rooms starting at $322">
             <Link to="/" className="btn-primary">
@@ -11,6 +14,8 @@ const Rooms = () => {
             </Link>
         </Banner>
         </Hero>
-    )
+        <RoomsContainer/>
+        </>
+    );
 }
 export  default Rooms;
